616697f1f9951b842dda2e1a7c8fb695cb186bba
alex <alex.ornberg@me.com>
2022-03-27 01:52:33 +0100

    remove unused types file

--- src/systems/dialogueSystem/components/interactionHandlers.d.ts
+++
@@ -1,9 +0,0 @@
-import { DialogueNode } from "../dialogues";
-import { CharacterInteraction } from "./characterInderaction";
-import DialogueManager from "./dialogueManager";
-
-export type InteractionHandler = (
-    node: DialogueNode,
-    interaction: CharacterInteraction,
-    manager: DialogueManager
-) => string | undefined;
